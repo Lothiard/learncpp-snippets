3edabed8bc0fecbd251b02e2bf9f04d732b57028
Restye János Barnabás <barna0801@gmail.com>
2025-07-29 12:43:13 +0200

    15.5

---
+++ 15.5/q1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+template <typename T, typename U, typename V>
+class Triad {
+public:
+    Triad (const T& first, const U& second, const V& third) :
+        first_{ first },
+        second_{ second },
+        third_{ third }
+    {}
+
+    const T& first() const { return first_; }
+    const U& second() const { return second_; }
+    const V& third() const { return third_; }
+
+    void print() const;
+
+private:
+    T first_{};
+    U second_{};
+    V third_{};
+};
+
+template <typename T, typename U, typename V>
+void Triad<T, U, V>::print() const {
+    std::cout << '[' << first() << ", " << second() << ", "
+              << third() << ']';
+}
+
+int main()
+{
+	Triad<int, int, int> t1{ 1, 2, 3 };
+	t1.print();
+	std::cout << '\n';
+	std::cout << t1.first() << '\n';
+
+	using namespace std::literals::string_literals;
+	const Triad t2{ 1, 2.3, "Hello"s };
+	t2.print();
+	std::cout << '\n';
+
+	return 0;
+}
