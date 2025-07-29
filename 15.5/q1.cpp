5c4e1f58e20b80773b0f4731c6a143d57c44aa35
Restye János Barnabás <barna0801@gmail.com>
2025-07-29 15:22:21 +0200

    15.7

--- 15.5/q1.cpp
+++ 15.5/q1.cpp
@@ -28,17 +28,16 @@ void Triad<T, U, V>::print() const {
               << third() << ']';
 }
 
-int main()
-{
-	Triad<int, int, int> t1{ 1, 2, 3 };
-	t1.print();
-	std::cout << '\n';
-	std::cout << t1.first() << '\n';
+int main() {
+    Triad<int, int, int> t1{ 1, 2, 3 };
+    t1.print();
+    std::cout << '\n';
+    std::cout << t1.first() << '\n';
 
-	using namespace std::literals::string_literals;
-	const Triad t2{ 1, 2.3, "Hello"s };
-	t2.print();
-	std::cout << '\n';
+    using namespace std::literals::string_literals;
+    const Triad t2{ 1, 2.3, "Hello"s };
+    t2.print();
+    std::cout << '\n';
 
-	return 0;
+    return 0;
 }
